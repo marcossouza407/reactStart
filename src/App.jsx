913398f7d16c2d724f7a29cf9aaa124e692746ca
marcossouza407 <116312153+marcossouza407@users.noreply.github.com>
2022-12-08 13:36:50 -0300

    feat : add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
-function App() {
-  
-
-  return (
-   <h1>hello world</h1> 
-
+import { Post } from "./Post"
+export function App() {
+ return (
+  <Post
+   author="eu mesmo"
+   content="dslfjkhvgdfhgljjfsljghslnvolshglnloshglh"
+   />
   )
 }
 
-export default App
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import styles from "./Header.module.css";
+
+export function Header(){
+    return (
+        <header classNome={styles.header}>
+        <strong>ignite feed</strong>
+        </header>
+        
+    )
+}
