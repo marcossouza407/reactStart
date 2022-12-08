567c0c0269e77253014e9c6a8d93bcfc2e7eec5b
marcossouza407 <116312153+marcossouza407@users.noreply.github.com>
2022-12-08 14:55:03 -0300

    feat:styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+
+import { Header } from "./components/header"
 import { Post } from "./Post"
 export function App() {
+
+
  return (
+  <>
+
+  <Header/>
   <Post
    author="eu mesmo"
    content="dslfjkhvgdfhgljjfsljghslnvolshglnloshglh"
    />
+   </>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import styles from "./Header.module.css";
 
 export function Header(){
     return (
-        <header classNome={styles.header}>
+        <header className={styles.header}>
         <strong>ignite feed</strong>
         </header>
         
     )
-}
+} 
