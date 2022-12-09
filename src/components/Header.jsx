bc094cfd3f28522521922e12cc9702b024a54cec
marcossouza407 <116312153+marcossouza407@users.noreply.github.com>
2022-12-09 17:14:34 -0300

    addLogoignite header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import styles from "./Header.module.css";
 
+import igniteLogo from '../assets/ignite-logo.svg';
 export function Header(){
     return (
         <header className={styles.header}>
-        <strong>ignite feed</strong>
+        <img sec={igniteLogo} alt="Logotipo do ignite"/>
+         
+    
         </header>
         
     )
