aa9498356d106914bf770f1ca927f833e1a4d5cb
marcossouza407 <116312153+marcossouza407@users.noreply.github.com>
2022-12-13 14:28:23 -0300

    Sidebar end

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -6,12 +7,16 @@ export function Sidebar() {
   <img className={styles.cover} src="https://images.unsplash.com/photo-1670163402343-2e01bda4a53e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"/>
   
   <div className={styles.profile}>
+    <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/116312153?v=4" />
     <strong>marcos</strong>
     <span> web developer</span>
   </div>
 
   <footer>
-    <a href="#"> Editar perfil</a>
+    <a href="#"> 
+    <PencilLine size={20}/>
+    Editar perfil
+    </a>
   </footer>
 
  </aside>
