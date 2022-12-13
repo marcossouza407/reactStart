e2f45b23d3153dea0275bb5c2b759bd44fab4def
marcossouza407 <116312153+marcossouza407@users.noreply.github.com>
2022-12-13 12:10:25 -0300

    Sidebar styles init

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-
+import { Sidebar } from "./components/Sidebar";
 import { Header } from "./components/header"
 import { Post } from "./Post"
+
+import styles from './App.module.css';
+
+import './global.css';
+
+
+
+
 export function App() {
 
 
  return (
-  <>
+  <div>
 
   <Header/>
-  <Post
+  <div className={styles.wrapper}>
+   <Sidebar/>
+    <main>
+    <Post
    author="eu mesmo"
    content="dslfjkhvgdfhgljjfsljghslnvolshglnloshglh"
    />
-   </>
+    </main>
+    </div>
+   </div>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import igniteLogo from '../assets/ignite-logo.svg';
 export function Header(){
     return (
         <header className={styles.header}>
-        <img sec={igniteLogo} alt="Logotipo do ignite"/>
+        <img src={igniteLogo} alt="Logotipo do ignite"/>
          
     
         </header>
